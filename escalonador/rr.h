cf35089a8be1b3707557523074f6b3b0c5a8d387
Júlia Rodrigues <105869092+juliarmn@users.noreply.github.com>
2024-05-31 16:34:12 -0300

    Create rr.h
    escalonador

---
+++ escalonador/rr.h
@@ -0,0 +1,36 @@
+#ifndef RR_H
+#define RR_H
+//#include "../Interface/interface.h"
+#include "../processo/processo.h"
+#include "../semaforo/semaforo.h"
+#include <locale.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <pthread.h>
+
+// Define do Quantum time padrao
+#define QUANTUM 5000
+
+// ====== STRUCTS =======
+
+// Struct da lista do escalonador na CPU
+typedef struct lista
+{
+    Processo *processo;
+    struct lista *prox;
+} Round_robin;
+
+// ==================================
+
+// ====== FUNCOES =======
+void inserir_round_robin(Round_robin **cabeca, Processo *elemento);
+int remover(Round_robin **cabeca, Processo *elemento);
+Round_robin *atender_instrucao(Round_robin *atendido, int quantum, Semaforo **cabeca_sem);
+Round_robin *atender_semaforo(Round_robin *atendido, Semaforo **cabeca_sem, char tipo);
+void robin_robin_atende(Round_robin **cabeca, Semaforo **cabeca_sem);
+void print_lista_robin(Round_robin *cabeca);
+
+#endif
